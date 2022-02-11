b9de2b7150b01e1ee2722dc6e92057f4ca328b0d
JFQ_DEV <jfqdev@gmail.com>
2022-02-10 21:00:45 -0300

    Add Mock categories

---
+++ src/components/categories/categoriesList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="q-ml-xs">
+        <q-card-section>
+            <q-toolbar-title>
+                Categorias
+            </q-toolbar-title>
+        </q-card-section>
+        <q-list  separator>
+            <q-item clickable v-ripple :active="active">
+                <q-item-section>Active</q-item-section>
+                <q-item-section avatar>
+                    <q-icon size="20px" color="primary" name="fas fa-arrow-right" />
+                </q-item-section>            
+            </q-item>
+        </q-list>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "categoriesList",
+    data(){
+        return {
+            data: "data"
+        }
+    }
+}
+</script>
