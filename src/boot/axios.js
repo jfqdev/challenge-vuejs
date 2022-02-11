c98e8e0f04b7da46fee2cc679ef34122e4d53356
JFQ_DEV <jfqdev@gmail.com>
2022-02-10 21:04:10 -0300

    axios base url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+const api = axios.create({ baseURL: 'http://sva.talana.com:8000/api/' })
+Vue.prototype.$api = api
+
+export { axios, api }
+
